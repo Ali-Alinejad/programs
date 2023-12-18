74fbc64fb4a6f23bb38511d8526e3e7de7c08fc5
WINFORD <winford.op@gmail.com>
2023-12-18 20:28:36 +0330

    test8:28

--- Ali`s JS/16-12-2023/js.js
+++ Ali`s JS/16-12-2023/js.js
@@ -10,27 +10,41 @@ const click = document.querySelector(".btn-conteiner");
     var data = await response.json();
     document.querySelector(".city").textContent = data.name;
     document.querySelector(".temp").textContent = Math.round(data.main.temp) + 'c';
-    document.querySelector(".wind").textContent ="windspeed:"+ data.wind.speed + "m/s" ;
-    document.querySelector(".deg").textContent ="deg:"+ data.wind.deg;
+    document.querySelector(".wind").textContent = data.wind.speed + "m/s" ;
+    document.querySelector(".deg").textContent = data.wind.deg;
     document.querySelector(".weather").textContent = data.weather[0].main;
-    document.querySelector(".humidity").textContent ="humidity:"+data.main.humidity+"%";
-    document.querySelector(".pressure").textContent = "pressure:"+data.main.pressure+"hPa";
-    Math.trunc(data.main.temp) + "c";
-    console.log(data);
+    document.querySelector(".humidity").textContent =data.main.humidity+"%";
+    document.querySelector(".pressure").textContent = data.main.pressure+"hPa";
+     Math.trunc(data.main.temp) + "c";
+     
+     
+     
     if (data.weather[0].main === "Clear") {
-      document.querySelector(".midl").style.background = "url('weather\ icons/clearn.png')";
+       document.querySelector(".midl").style.background = "url('weather\ icons/clearn.png')";
+       document.querySelector(".midl").style.backgroundPosition = "center";  
+       document.querySelector(".midl").style.backgroundSize = "cover"; 
     }
     else if (data.weather[0].main === "Clouds") {
        document.querySelector(".midl").style.background = "url('weather\ icons/cloudn.png')";
+        document.querySelector(".midl").style.backgroundPosition = "center";  
+       document.querySelector(".midl").style.backgroundSize = "cover"; 
+       
+    
     }
  else if (data.weather[0].main === "Snow") {
        document.querySelector(".midl").style.background = "url('weather\ icons/snowyy.png')";
+        document.querySelector(".midl").style.backgroundPosition = "center";  
+       document.querySelector(".midl").style.backgroundSize = "cover"; 
     }
     else if (data.weather[0].main === "Rain") {
        document.querySelector(".midl").style.background = "url('weather\ icons/Rainyy.png')";
+        document.querySelector(".midl").style.backgroundPosition = "center";  
+       document.querySelector(".midl").style.backgroundSize = "cover"; 
      }
      else if (data.weather[0].main === "Haze") {
        document.querySelector(".midl").style.background = "url('weather\ icons/haze.png')";
+        document.querySelector(".midl").style.backgroundPosition = "center";  
+       document.querySelector(".midl").style.backgroundSize = "cover"; 
     }
 
 }
